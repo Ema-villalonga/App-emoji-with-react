3f629c156768a1ca43d1514018bf0bb109c4ec97
Angel Emanuel Villalonga <88913610+Ema-villalonga@users.noreply.github.com>
2022-05-14 21:16:41 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import EmojiPickerInput from "./components/emojis/emojiPickerInput";
+
+export default function App() {
+  return (
+    <div className="App">
+      <EmojiPickerInput />
+    </div>
+  );
+}
---
+++ src/components/emojis/data.js
@@ -0,0 +1,147 @@
+export const data = [
+  {
+    symbol: "😄",
+    name: "cara alegre",
+    keywords: "smiling face happy"
+  },
+  {
+    symbol: "💓",
+    name: "corazon",
+    keywords: "heart love like"
+  },
+  {
+    symbol: "😎",
+    name: "genial",
+    keywords: "brilliant great"
+  },
+  {
+    symbol: "😜",
+    name: "broma",
+    keywords: "joke play smiling"
+  },
+  {
+    symbol: "😠",
+    name: "enojo",
+    keywords: "angry fury"
+  },
+  {
+    symbol: "😲",
+    name: "asombro",
+    keywords: "astonishment"
+  },
+  {
+    symbol: "😭",
+    name: "llorar",
+    keywords: "cry"
+  },
+  {
+    symbol: "😒",
+    name: "aburrido",
+    keywords: "bored"
+  },
+  {
+    symbol: "😈",
+    name: "malvado",
+    keywords: "wicked"
+  },
+  {
+    symbol: "😘",
+    name: "besos",
+    keywords: "kiss"
+  },
+  {
+    symbol: "👊",
+    name: "puño",
+    keywords: "fist"
+  },
+  {
+    symbol: "👀",
+    name: "curiosidad",
+    keywords: "curiosity"
+  },
+  {
+    symbol: "😔",
+    name: "callado",
+    keywords: "reserved"
+  },
+  {
+    symbol: "💣",
+    name: "bomba",
+    keywords: "bomb burst"
+  },
+  {
+    symbol: "🔥",
+    name: "fuego",
+    keywords: "fire hot burn"
+  },
+  {
+    symbol: "😇",
+    name: "angelical",
+    keywords: "angel"
+  },
+  {
+    symbol: "😍",
+    name: "enamorado",
+    keywords: "love"
+  },
+  {
+    symbol: "🚉",
+    name: "subte",
+    keywords: "underground"
+  },
+  {
+    symbol: "🚏",
+    name: "colectivo",
+    keywords: "omnibus"
+  },
+  {
+    symbol: "🚑",
+    name: "ambulancia",
+    keywords: "ambulance same"
+  },
+  {
+    symbol: "🚒",
+    name: "bomberos",
+    keywords: "firefighter fire"
+  },
+  {
+    symbol: "🚗",
+    name: "auto",
+    keywords: "car"
+  },
+  {
+    symbol: "🚚",
+    name: "camion",
+    keywords: "buzz"
+  },
+  {
+    symbol: "🚢",
+    name: "barco",
+    keywords: "yate ship"
+  },
+  {
+    symbol: "👋",
+    name: "saludo",
+    keywords: "greetings hello"
+  },
+  {
+    symbol: "👍",
+    name: "ok",
+    keywords: "okey excelent"
+  },
+  {
+    symbol: "🔫",
+    name: "revolver",
+    keywords: "gun"
+  },
+  {
+    symbol: "🔒",
+    name: "candado",
+    keywords: "close"
+  },
+  {
+    symbol: "📱",
+    name: "celular",
+    keywords: "cell phone"
+  }
+];
